06aefe4898e9af4d9639a2db06918e2a5bee3c32
Maxwell Olmen <mathematickid@gmail.com>
2023-03-08 03:59:42 +0000

    Updated !help

--- src/main/java/com/maxwellolmen/grassbot/command/HelpCommand.java
+++ src/main/java/com/maxwellolmen/grassbot/command/HelpCommand.java
@@ -4,9 +4,12 @@ import com.maxwellolmen.grassbot.handler.Command;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.awt.Color;
 
 public class HelpCommand implements Command {
     
@@ -19,12 +22,17 @@ public class HelpCommand implements Command {
 
     @Override
     public void onCommand(MessageReceivedEvent event, String command) {
-        String message = "GrassBot Commands:\n";
+        StringBuilder sb = new StringBuilder();
         
         for (Map.Entry<String, Command> set : commands.entrySet()) {
-            message += "!" + set.getKey() + "\n";
+            sb.append("!").append(set.getKey()).append("\n");
         }
         
-        event.getChannel().sendMessage(message).queue();
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setTitle("GrassBot Commands");
+        eb.setColor(Color.BLUE);
+        eb.setDescription(sb.toString());
+
+        event.getChannel().sendMessageEmbeds(eb.build()).queue();
     }
 }
