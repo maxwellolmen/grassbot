6af287b0ed7d6be44727d514f2b58f45cf3f8166
Maxwell Olmen <mathematickid@gmail.com>
2023-10-09 21:09:04 -0500

    Final date and time fix

--- src/main/java/com/maxwellolmen/grassbot/handler/ScheduleHandler.java
+++ src/main/java/com/maxwellolmen/grassbot/handler/ScheduleHandler.java
@@ -36,10 +36,13 @@ public class ScheduleHandler {
             newCount = TouchGrassCommand.scaleCount(randomId, 1 - percentage);
 
             channel.sendMessage("The time is here! Congratulations <@" + randomId
-                    + ">, your grass count has been reduced from " + oldCount + " to " + newCount + " for a decrease of "
+                    + ">, your grass count has been reduced from " + oldCount + " to " + newCount
+                    + " for a decrease of "
                     + percentage
                     + "%! See you next Monday @ 9pm Central, where a random user will be selected for a random decrease in their grasscount :)")
                     .queue();
+
+            initLottery();
         }
     }
 
@@ -53,16 +56,24 @@ public class ScheduleHandler {
         Timer timer = new Timer();
 
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-        calendar.set(Calendar.HOUR_OF_DAY, 21);
+
+        int daysUntilNextMonday = Calendar.MONDAY - calendar.get(Calendar.DAY_OF_WEEK);
+
+        // If today is already Monday and it's past 9 PM, add 7 days to get to the next
+        // Monday
+        if (daysUntilNextMonday <= 0 && calendar.get(Calendar.HOUR_OF_DAY) >= 21) {
+            daysUntilNextMonday += 7;
+        }
+
+        // Add the calculated days to get to the next Monday
+        calendar.add(Calendar.DAY_OF_WEEK, daysUntilNextMonday);
+
+        // Set the time to 9 PM
+        calendar.set(Calendar.HOUR_OF_DAY, 21); // 9 PM
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
 
         Date timeToRun = calendar.getTime();
-        
-        if (System.currentTimeMillis() > timeToRun.getTime()) {
-            timeToRun.setTime(timeToRun.getTime() + 86400000);
-        }
 
         timer.schedule(lotteryTask, timeToRun);
     }
